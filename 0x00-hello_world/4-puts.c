a734de65bddee475ed0cdb48dc2374f7f7093042
hyperbayse <hyperbayse@gmail.com>
2023-09-10 01:18:06 -0700

    program update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
+/*
+ * program that prints the text in puts
+ */
 #include <stdio.h>
-/* it prints the word placed in puts function */
+/*
+ * it prints the word placed in puts function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
