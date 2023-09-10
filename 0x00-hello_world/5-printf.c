b80320d7977d7ad71f41fce1507aff292d7fedef
hyperbayse <hyperbayse@gmail.com>
2023-09-10 15:00:42 -0700

    program updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+ * File: 5-printf.c
+ */
+#include <stdio.h>
+/**
+ * main - prints the texts placed in printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0)
+}
