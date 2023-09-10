52c555b6e5d10f315d039c3cdd9106481fa1aead
hyperbayse <hyperbayse@gmail.com>
2023-09-10 15:46:42 -0700

    program updated

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+/*
+ * File: 6-size.c
+ */
+#include <stdio.h>
+/**
+ * main - it prints the size of types on computer
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long -int));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
