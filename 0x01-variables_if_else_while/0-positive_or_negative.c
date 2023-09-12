a51133349149ff2f2489fe30c3d097ba7f69e48d
hyperbayse <hyperbayse@gmail.com>
2023-09-11 19:12:51 -0700

    program updated

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-positive_or_negative.c
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints a random number
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
