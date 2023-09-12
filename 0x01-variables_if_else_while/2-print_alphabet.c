fd8b5560527492a0191ed58d20ed3d366fd9f9bd
hyperbayse <hyperbayse@gmail.com>
2023-09-11 19:51:53 -0700

    program updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,19 @@
  */
 #include <stdio.h>
 /**
- * main -Prints th alphabet
+ * main -Prints the alphabet
  * Return: 0
  */
 int main(void)
 {
 	char letter;
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
+	}
+
 	putchar('\n');
+
 	return (0);
 }
