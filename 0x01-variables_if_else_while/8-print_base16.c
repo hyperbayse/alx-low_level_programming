f72dfc49ccdda16c00286d77f6718e808153ccac
hyperbayse <hyperbayse@gmail.com>
2023-09-11 21:08:31 -0700

    program updated

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+/*
+ * File: 8-print_base16.c
+ */
+#include <stdio.h>
+/**
+ * main - Prints hexadecimal string
+ */
+int main(void)
+{
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
