912c0fd1f2b8395a9f0e5acc90690bf9230d70cb
hyperbayse <hyperbayse@gmail.com>
2023-09-13 18:36:40 -0700

    program updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - runs the program
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	printf("putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project is based on functions and nested loops
