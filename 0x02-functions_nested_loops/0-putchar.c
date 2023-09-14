4bf19c287ec26304b5184c2b279e2dc6bd1db5f6
hyperbayse <hyperbayse@gmail.com>
2023-09-13 18:57:43 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - runs the program
  * Return: 0
@@ -6,6 +6,12 @@
 int main(void)
 {
 	_putchar('_');
-	printf("putchar");
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
