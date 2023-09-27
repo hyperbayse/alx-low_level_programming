f3e1aa7fcfdb7c81a2df69d6038775d27241e737
hyperbayse <hyperbayse@gmail.com>
2023-09-26 17:33:05 -0700

    update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets string pointer
+ * @s: value modifyer
+ * @to: value to assign
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
