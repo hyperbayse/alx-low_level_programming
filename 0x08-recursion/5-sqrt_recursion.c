574950665b0faae7c19b3afce1f4b9c3c0f1d9bf
hyperbayse <hyperbayse@gmail.com>
2023-10-01 15:20:09 -0700

    update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _sqrt - calculates suarte root
+ * @n: number input
+ * @i: iterator
+ * Return: square result
+ */
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+	if (sqrt > n) 
+		return (-1);
+	if (sqrt == n)
+		return (i);
+	return (_sqrt(n, 1 + 1));
+}
