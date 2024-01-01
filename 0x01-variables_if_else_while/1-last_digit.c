c1c44b7ec61837721b04f1a5151e8d89eded6b96
hyperbayse <hyperbayse@gmail.com>
2023-12-31 22:43:02 -0800

    1-last_digit.c update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,39 @@
 /*
- * File: 1-last_digit.c 
+ * File: 1-last_digit.c
+ * Desc: It checkts if the last number of the randomly generated integers is < 5,  =0 or < 6 && > 0
+ * Author: absheriff
  */
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/**
- * main - Print last number
- * Return: 0
+#include <string.h>
+
+/*
+ * main - prints the last digit of the randomly generated numbers and check for some conditions
  */
-int main(void)
-{
-	int n;
+int main(){
+	int n, last;
+
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n %10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is $d and is 0\n", n, n % 10);
-	}
-	return (0);
+	n = rand() - (RAND_MAX/2);
+
+	//the number of string generated is saved inside gstr;
+	char gstr[20];
+
+	sprintf(gstr, "%d", n);
+	last = gstr[strlen(gstr)-1];
+	last = last - '0';
+	
+	if(n<0) last = last * -1;
+
+	if(last > 5) 
+		printf("Last digit of %d is %d and is greater than 5", n, last);
+	else if(last == 0) 
+		printf("Last digit of %d is %d and is 0", n, last);
+	else if(last < 6 && last != 0) 
+		printf("Last digit of %d is %d  and is less than 6 and not 0", n, last);
+
+	printf("\n");
 }
+
