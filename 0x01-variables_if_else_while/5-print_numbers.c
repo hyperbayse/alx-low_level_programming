b57c5dcab475586677b59269f69e82ed669107db
hyperbayse <hyperbayse@gmail.com>
2024-01-01 00:17:42 -0800

    5-print_numbers.c updated

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,22 @@
+/*
+ * 5-print_numbers.c
+ *
+ *  Created on: Dec 31, 2023
+ *      Author: absheriff
+ */
+
 #include <stdio.h>
+
 /**
- * main - Prints numbers range
- * Return: Always (Success)
+ * main - Prints digit from 0 - 9
+ * Return: 0
  */
 int main(void)
 {
-	char n;
-	for (n = '0'; n <= '9'; n++)
-	{
-		putchar(n);
-	}
+	int n;
+
+	for(n = 0; n < 10; n++)
+		printf("%d", n);
 	putchar('\n');
 	return (0);
 }
