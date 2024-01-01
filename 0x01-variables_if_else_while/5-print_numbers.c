78ae644c16e3e4f994f6716153c01bc29d0656fa
hyperbayse <hyperbayse@gmail.com>
2024-01-01 00:20:09 -0800

    5-print_numbers.c updated

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,12 +11,12 @@
  * main - Prints digit from 0 - 9
  * Return: 0
  */
-int main(void)
+int main (void)
 {
 	int n;
 
-	for(n = 0; n < 10; n++)
-		printf("%d", n);
-	putchar('\n');
+	for (n = 0; n < 10; n++)
+		printf ("%d", n);
+	putchar ('\n');
 	return (0);
 }
