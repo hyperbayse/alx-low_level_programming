7e6b03d8240eefed6b1cd1e23cf62766bb633e0c
hyperbayse <hyperbayse@gmail.com>
2024-01-07 05:53:42 -0800

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+ * 7-print_last_digit.c
+ *
+ *  Created on: Jan 7, 2024
+ *      Author: absheriff
+ */
+
+/**
+ * print_last_digit - prints the last digit
+ * Return: success
+ * @i: integer value
+ * r: return value
+ */
+
+int print_last_digit(int i)
+{
+	int r;
+
+	if (i < 0)
+	{
+		i = -(i);
+	}
+	r = i % 10;
+	_putchar(r + '0');
+	return (r);
+}
